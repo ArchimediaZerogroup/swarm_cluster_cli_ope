88fd6dd0a981941c751fab3128b43dada5a5b8c5
Marino Bonetti <marinobonetti@gmail.com>
2020-09-10 17:35:31 +0200

    modifiche per miglior codice e sistema di disconnessione corretto

--- lib/swarm_cluster_cli_ope/sync_configs/post_gres.rb
+++ lib/swarm_cluster_cli_ope/sync_configs/post_gres.rb
@@ -2,28 +2,27 @@ module SwarmClusterCliOpe
   module SyncConfigs
     class PostGres < BaseDatabase
 
-      # @return [TrueClass, FalseClass]
+
       def pull
         resume('pull')
 
-        dump_cmd = dump_cmd(remote.username, remote.password, remote.database_name)
-        logger.info{ "DUMP  COMMAND: #{dump_cmd.join(' ')}"}
         if yes?("Confermare il comando?[y,yes]")
+
           tmp_file = "/tmp/#{Time.now.to_i}.sql.gz"
-          container.exec("bash -c '#{dump_cmd.join(' ')}' > #{tmp_file}")
-          local_container.copy_in(tmp_file, tmp_file)
+          dump_cmd(remote, tmp_file)
+          local.container.copy_in(tmp_file, tmp_file)
 
           # drop old db and recreate
-          drop_cmd = drop_cmd(local.username, local.password, local.database_name)
-          logger.info{ "DROP  COMMAND: #{drop_cmd.join(' ')}"}
-          local_container.exec("bash -c '#{drop_cmd.join(' ')}'")
-          create_cmd = create_cmd(local.username, local.password, local.database_name)
-          logger.info{ "CREATE COMMAND: #{create_cmd.join(' ')}"}
-          local_container.exec("bash -c '#{create_cmd.join(' ')}'")
+          if Gem::Version.new(local.database_version) <= Gem::Version.new("12")
+            close_connections_and_drop_cmd(local)
+          else
+            raise "DA ANALIZZARE QUANDO LA 13 disponibile....dropdb ha un force come parametro"
+          end
 
-          restore_cmd = restore_cmd(local.username, local.password, local.database_name, tmp_file)
-          logger.info{ "RESTORE COMMAND: #{restore_cmd.join(' ')}"}
-          local_container.exec("bash -c '#{restore_cmd.join(' ')}'")
+          create_cmd(local)
+
+          restore_cmd(local, tmp_file)
+
         end
         true
       end
@@ -32,25 +31,22 @@ module SwarmClusterCliOpe
       def push
         resume('PUSH')
 
-        dump_cmd = dump_cmd(local.username, local.password, local.database_name)
-        say "DUMP COMMAND: #{dump_cmd.join(' ')}"
         if yes?("ATTENZIONE !!!!!!PUSH!!!!! - Confermare il comando?[y,yes]")
+
           tmp_file = "/tmp/#{Time.now.to_i}.sql.gz"
-          local_container.exec("bash -c '#{dump_cmd.join(' ')}' > #{tmp_file}")
-          container.copy_in(tmp_file, tmp_file)
+          dump_cmd(local, tmp_file)
+          remote.container.copy_in(tmp_file, tmp_file)
 
           # drop old db and recreate
-          drop_cmd = drop_cmd(remote.username, remote.password, remote.database_name)
-          logger.info{ "DROP  COMMAND: #{drop_cmd.join(' ')}"}
-          container.exec("bash -c '#{drop_cmd.join(' ')}'")
+          if Gem::Version.new(local.database_version) <= Gem::Version.new("12")
+            close_connections_and_drop_cmd(remote)
+          else
+            raise "DA ANALIZZARE QUANDO LA 13 disponibile....dropdb ha un force come parametro"
+          end
+          create_cmd(remote)
 
-          create_cmd = create_cmd(remote.username, remote.password, remote.database_name)
-          logger.info{ "CREATE COMMAND: #{create_cmd.join(' ')}"}
-          container.exec("bash -c '#{create_cmd.join(' ')}'")
+          restore_cmd(remote, tmp_file)
 
-          restore_cmd = restore_cmd(remote.username, remote.password, remote.database_name, tmp_file)
-          say "RESTORE COMMAND: #{restore_cmd.join(' ')}"
-          container.exec("bash -c '#{restore_cmd.join(' ')}'")
         end
         true
       end
@@ -69,47 +65,93 @@ module SwarmClusterCliOpe
 
       private
 
-      def create_cmd(username, password, database_name)
+      # @param [EnvConfigs] config
+      def create_cmd(config)
         create_cmd = []
-        create_cmd << "PGPASSWORD=\"#{password}\""
+        create_cmd << "PGPASSWORD=\"#{config.password}\""
         create_cmd << 'createdb'
-        create_cmd << "--username=#{username}"
-        create_cmd << database_name
+        create_cmd << "--username=#{config.username}"
+        create_cmd << config.database_name
+
+        logger.info { "CREATE COMMAND: #{create_cmd.join(' ')}" }
+        config.container.exec("bash -c '#{create_cmd.join(' ')} || true'")
       end
 
-      def drop_cmd(username, password, database_name)
-        drop_cmd = []
-        drop_cmd << "PGPASSWORD=\"#{password}\""
-        drop_cmd << 'dropdb'
-        drop_cmd << "--username=#{username}"
-        drop_cmd << database_name
-        drop_cmd
-      end
+      # @param [EnvConfigs] config
+      # def drop_cmd(config)
+      #   drop_cmd = []
+      #   drop_cmd << "PGPASSWORD=\"#{config.password}\""
+      #   drop_cmd << 'dropdb'
+      #   drop_cmd << '--if-exists'
+      #   drop_cmd << "--username=#{config.username}"
+      #   drop_cmd << config.database_name
+      #   drop_cmd
+      #
+      #   logger.info { "DROP  COMMAND: #{drop_cmd.join(' ')}" }
+      #   config.container.exec("bash -c '#{drop_cmd.join(' ')}'")
+      # end
 
-      def restore_cmd(username, password, database_name, tmp_file)
+      # @param [EnvConfigs] config
+      def restore_cmd(config, tmp_file)
         restore_cmd = []
-        restore_cmd << "PGPASSWORD=\"#{password}\""
+        restore_cmd << "PGPASSWORD=\"#{config.password}\""
         restore_cmd << 'pg_restore'
         restore_cmd << '--no-acl'
         restore_cmd << '--no-owner'
-        restore_cmd << "--username=#{username}"
-        restore_cmd << "--dbname=#{database_name}"
+        restore_cmd << "--username=#{config.username}"
+        restore_cmd << "--dbname=#{config.database_name}"
         restore_cmd << tmp_file
         restore_cmd
+
+        logger.info { "RESTORE COMMAND: #{restore_cmd.join(' ')}" }
+        config.container.exec("bash -c '#{restore_cmd.join(' ')}'")
       end
 
-      def dump_cmd(username, password, database_name)
+      # @param [EnvConfigs] config
+      def dump_cmd(config, file)
         dump_cmd = []
-        dump_cmd << "PGPASSWORD=\"#{password}\""
+        dump_cmd << "PGPASSWORD=\"#{config.password}\""
         dump_cmd << 'pg_dump'
         dump_cmd << '--no-acl'
         dump_cmd << '--no-owner'
-        dump_cmd << "--username=#{username}"
+        dump_cmd << "--username=#{config.username}"
         dump_cmd << '--format=custom'
         dump_cmd << '--compress=9'
-        dump_cmd << database_name
+        dump_cmd << config.database_name
         dump_cmd
+
+        logger.info { "DUMP  COMMAND: #{dump_cmd.join(' ')}" }
+        config.container.exec("bash -c '#{dump_cmd.join(' ')}' > #{file}")
+
       end
+
+
+      # @param [EnvConfigs] config
+      def close_connections_and_drop_cmd(config)
+        cmd = []
+        cmd << "PGPASSWORD=\"#{config.password}\""
+
+        sql = []
+        sql << "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname =  '\"'\"'#{config.database_name}'\"'\"' AND pid <> pg_backend_pid();;"
+        sql << "DROP DATABASE IF EXISTS #{config.database_name};"
+
+        cmd << "echo \"#{sql.join(" ")}\" "
+        cmd << '|'
+        cmd << 'psql'
+        cmd << "-U #{config.username}"
+        cmd << "postgres"
+        cmd
+
+        logger.info { "CLOSE CONNECTIONS COMMAND: #{cmd.join(' ')}" }
+        config.container.exec("bash -c '#{cmd.join(' ')}'")
+      end
+
+      # quello che fa capistrano quando copia in locale - utenze inventate
+      # gzip -d cortobio_production_new_2020-09-10-171742.sql.gz  &&
+      #   PGPASSWORD='root' psql -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'development' AND pid <> pg_backend_pid();;"  -U root  -h 0.0.0.0  -p 32790  development;
+      # PGPASSWORD='root' dropdb  -U root  -h 0.0.0.0  -p 32790  development;
+      # PGPASSWORD='root' createdb  -U root  -h 0.0.0.0  -p 32790  development;
+      # PGPASSWORD='root' psql  -U root  -h 0.0.0.0  -p 32790  -d development < ./cortobio_production_new_2020-09-10-171742.sql
 
 
     end
