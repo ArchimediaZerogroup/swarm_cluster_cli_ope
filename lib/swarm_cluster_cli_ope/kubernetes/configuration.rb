12704ba6a7b1c60cb2c252be3ee4cd673c62ae26
Marino Bonetti <marinobonetti@gmail.com>
2020-11-09 21:54:14 +0100

    configurazione base per kubernetes

--- lib/swarm_cluster_cli_ope/kubernetes/configuration.rb
+++ lib/swarm_cluster_cli_ope/kubernetes/configuration.rb
@@ -3,6 +3,23 @@ module SwarmClusterCliOpe
     class Configuration < BaseConfiguration
 
 
+      ##
+      # In kubernetes abbiamo il context, il context può essere ricevuto o dalla configurazione oppure dal current_context
+      # di kubelet
+      # @return [String]
+      def context
+        cmd = ShellCommandExecution.new(['kubectl config current-context'])
+        cmd.execute.raw_result[:stdout]
+      end
+
+      ##
+      # Salva le configurazioni base in HOME
+      def save_base_cfgs
+        super do |obj|
+          obj.merge({connections_maps: {context: context}})
+        end
+      end
+
 
     end
   end
