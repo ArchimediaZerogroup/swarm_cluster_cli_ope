4f93e1a7907be5aa52497a268055e4031b0f8a2b
Marino Bonetti <marinobonetti@gmail.com>
2020-11-09 21:24:33 +0100

    trasformazione in concern di rails

--- lib/swarm_cluster_cli_ope/configuration_concern.rb
+++ lib/swarm_cluster_cli_ope/configuration_concern.rb
@@ -1,8 +1,14 @@
+require 'active_support/concern'
+
 module SwarmClusterCliOpe
   module ConfigurationConcern
+    extend ActiveSupport::Concern
 
-    def self.included(klass)
-      klass.extend(ClassMethods)
+    included do
+      # @return [SwarmClusterCliOpe::Configuration]
+      def cfgs
+        self.class.cfgs
+      end
     end
 
     module ClassMethods
@@ -13,12 +19,5 @@ module SwarmClusterCliOpe
         Configuration.instance
       end
     end
-
-
-    # @return [SwarmClusterCliOpe::Configuration]
-    def cfgs
-      self.class.cfgs
-    end
-
   end
 end
