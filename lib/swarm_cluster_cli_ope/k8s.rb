65a1f6a4ddc00278ab9d275d015ddc36f69afbc0
Marino Bonetti <marinobonetti@gmail.com>
2020-11-09 19:33:29 +0100

    implementazione rsync con pod

--- lib/swarm_cluster_cli_ope/k8s.rb
+++ lib/swarm_cluster_cli_ope/k8s.rb
@@ -16,20 +16,18 @@ module SwarmClusterCliOpe
     def rsync(src, dst)
       if yes? "Attenzione, i dati locali o remoti verranno sovrascritti/cancellati?[y,yes]"
 
-        direction = :download
         reg_exp = /(?<pod_name>.*)\:(?<path>.*)/
         if File.exist?(src)
           # il src é la cartella, quindi la destizione è il pod
           direction = :upload
-          src_pod = nil
-          src_path = src
-          podname = dst_pod = dst.match(reg_exp)[:pod_name]
-          dst_path = dst.match(reg_exp)[:path]
+          local_path = src
+          podname = dst.match(reg_exp)[:pod_name]
+          podpath = dst.match(reg_exp)[:path]
         else
-          podname = src_pod = src.match(reg_exp)[:pod_name]
-          src_path = src.match(reg_exp)[:path]
-          dst_pod = nil
-          dst_path = dst
+          direction = :download
+          podname = src.match(reg_exp)[:pod_name]
+          podpath = src.match(reg_exp)[:path]
+          local_path = dst
         end
 
         puts "#{src} #{direction} #{dst}"
@@ -41,23 +39,13 @@ module SwarmClusterCliOpe
 
           puts "Stiamo utilizzando il contesto: #{context.api_endpoint}"
 
-          # cli = Kubeclient::Client.new(
-          #   context.api_endpoint,
-          #   'v1',
-          #   ssl_options: context.ssl_options,
-          #   auth_options: context.auth_options
-          # )
-          #
-          #
-          # cli.exec(namespace:cfgs.stack_name)
-
           base_cmd = ["kubectl", "-n #{cfgs.stack_name}"]
 
           cmd = ShellCommandExecution.new([*base_cmd, "exec #{podname}", "--", 'bash -c "apt update && apt install -yq rsync psmisc"'])
           if cmd.execute.failed?
             puts "Problemi nell'installazione di rsync nel pod"
           else
-            cmd = ShellCommandExecution.new([*base_cmd, "cp", File.expand_path("../k8s_rsync/rsyncd.conf", __FILE__ ), "#{podname}:/tmp/."])
+            cmd = ShellCommandExecution.new([*base_cmd, "cp", File.expand_path("../k8s_rsync/rsyncd.conf", __FILE__), "#{podname}:/tmp/."])
             copy_1 = cmd.execute.failed?
             cmd = ShellCommandExecution.new([*base_cmd, "cp", File.expand_path("../k8s_rsync/rsyncd.secrets", __FILE__), "#{podname}:/tmp/."])
             copy_2 = cmd.execute.failed?
@@ -68,31 +56,35 @@ module SwarmClusterCliOpe
             else
 
 
-              # p= IO.popen([*base_cmd, "exec -it #{podname}", "--", 'bash -c "rsync --daemon --config=/tmp/rsyncd.conf  --verbose --log-file=/tmp/rsync.log"'].join(" "))
-              # pid= p.pid
-              # puts "PID in execuzione:#{pid}"
-              # sleep 10
-              # Process.kill("INT", pid)
-
               cmd = ShellCommandExecution.new([*base_cmd, "exec -i #{podname}", "--", 'bash -c "rsync --daemon --config=/tmp/rsyncd.conf  --verbose --log-file=/tmp/rsync.log"'])
               if cmd.execute.failed?
                 puts "Rsync non startato"
               else
-                # kubectl -n $NAMESPACE port-forward $PODNAME 10873:873
-
                 local_port = rand(30000..40000)
 
-                 p= IO.popen([*base_cmd, "port-forward #{podname} #{local_port}:873"].join(" "))
-                 pid= p.pid
-                 puts "PID in execuzione port forward:#{pid}"
+                p = IO.popen([*base_cmd, "port-forward #{podname} #{local_port}:873"].join(" "))
+                pid = p.pid
+                puts "PID in execuzione port forward:#{pid}"
 
                 sleep 1
 
                 # lanciamo il comando quindi per far rsync
+                rsync_command = [
+                  "rsync -az --no-o --no-g",
+                  "--delete",
+                  "--password-file=#{ File.expand_path("../k8s_rsync/password", __FILE__)}"
+                ]
 
-                # rsync -avz --no-o --no-g --password-file=./password /PATH_CARTELLA_LOCALE rsync://root@0.0.0.0:10873/archives/PATH_REMOTA
+                if direction == :upload
+                  rsync_command << local_path
+                  rsync_command << "rsync://root@0.0.0.0:#{local_port}/archives#{podpath}"
+                else
+                  rsync_command << "rsync://root@0.0.0.0:#{local_port}/archives#{podpath}"
+                  rsync_command << local_path
+                end
 
-                cmd = ShellCommandExecution.new("rsync -avz --no-o --no-g --password-file=#{ File.expand_path("../k8s_rsync/password", __FILE__ )} #{src_path} rsync://root@0.0.0.0:#{local_port}/archives#{dst_path}")
+
+                cmd = ShellCommandExecution.new(rsync_command)
                 cmd.execute
 
                 sleep 1
@@ -106,11 +98,6 @@ module SwarmClusterCliOpe
                 cmd.execute
 
               end
-              #
-              # puts t.inspect
-              # sleep 10
-              #
-              # cmd.kill
 
             end
 
