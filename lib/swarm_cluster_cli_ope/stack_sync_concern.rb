950797c7baf9d7e703f8ca808034742a027e8332
Marino Bonetti <marinobonetti@gmail.com>
2021-03-04 11:53:54 +0100

    documentazione nella cli mancante

--- lib/swarm_cluster_cli_ope/stack_sync_concern.rb
+++ lib/swarm_cluster_cli_ope/stack_sync_concern.rb
@@ -66,7 +66,19 @@ module SwarmClusterCliOpe
         - pg_user: "postgres"                   -> valore in chiaro, in sostituzione della variabile ambiente, DEFAULT: postgres
         - database_name_env: "POSTGRES_DB"      -> variabile ambiente interna al servizio contenente NOME DB, DEFAULT: POSTGRES_DB         
         - database_name: "nome_db"              -> valore in chiaro, in sostituzione della variabile ambiente     
-              
+      mongo:
+      -- local:  -> hash di configurazioni per il DB locale
+        - service: "db"                         -> nome del servizio nel compose locale, DEFAULT: quello definito sopra
+        - password: ""                          -> password per DB
+        - username: ""                          -> username per DB
+        - database_name: "nome_db"              -> nome del DB
+        - exclude_from_sync: "coll1,coll2"      -> elenco di collections di cui non eseguire il dump quando facciamo PUSH, DEFAULT: ''
+      -- remote: -> hash di configurazioni per il DB remoto
+        - service: "db"                         -> nome del servizio nel compose locale, DEFAULT: quello definito sopra
+        - password: ""                          -> password per DB
+        - username: ""                          -> username per DB
+        - database_name: "nome_db"              -> nome del DB
+        - exclude_from_sync: "coll1,coll2"      -> elenco di collections di cui non eseguire il dump quando facciamo PULL, DEFAULT: ''        
 
       EXAMPLE:
       Esempio di sincronizzazione di un file sqlite3 e una cartella
