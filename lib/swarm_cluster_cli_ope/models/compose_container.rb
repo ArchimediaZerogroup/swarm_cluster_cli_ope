ec47538784fde7f036a3f596481c1b0b4f427540
Marino Bonetti <marinobonetti@gmail.com>
2022-06-24 16:21:17 +0200

    correzione estrapolazione nome servizio rispetto a docker compose
    related #13

--- lib/swarm_cluster_cli_ope/models/compose_container.rb
+++ lib/swarm_cluster_cli_ope/models/compose_container.rb
@@ -4,7 +4,14 @@ module SwarmClusterCliOpe
 
       # @return [SwarmClusterCliOpe::Models::ComposeContainer]
       def self.find_by_service_name(service_name, stack_name: '')
-        res = ShellCommandExecution.new("docker inspect #{[stack_name, service_name, "1"].compact.join("_".strip)}").execute
+        # prima controlliamo se siamo con il vecchio sistema di docker-compose dove i nomi dei servizi venivano
+        # costruiti con _ oppure se siamo nella nuova versione di docker compose dove c'è il - che fà da spaziatore
+        res = nil
+        ["_", "-"].each do |separatore|
+          res = ShellCommandExecution.new("docker inspect #{[stack_name, service_name, "1"].compact.join(separatore)}").execute(allow_failure: true)
+          break unless res.failed?
+        end
+        raise "Non siamo riusciti ad identificare il servizio in locale" if res.failed?
         self.new(JSON.parse(res.raw_result[:stdout]).first)
       end
 
